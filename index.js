fba7983bde0740aaaf229a52070fd7ab89057446
Andrew S. Brown <andrew@launchdarkly.com>
2018-07-25 15:29:42 -0700

    Remove rule ids prior to generating json patches.
    Rule ids are read-only unless you are just moving rules around so we need to
    stop sending them becaues they cause the patch to be rejected.

--- index.js
+++ index.js
@@ -59,6 +59,16 @@ var copyValues = function (flag, destinationEnvironment, sourceEnvironment) {
   });
 }
 
+var stripRuleIds = function(flag) {
+  for (let env in flag.environments) {
+    if(!flag.environments.hasOwnProperty(env)) continue;
+
+    for (let rule of flag.environments[env].rules) {
+      delete rule._id;
+    }
+  }
+}
+
 function syncEnvironment (fromKey, toKey) {
   fetchFlags(function (err, flags) {
     if (err) {
@@ -66,6 +76,7 @@ function syncEnvironment (fromKey, toKey) {
     }
 
     flags.forEach(function (flag) {
+      stripRuleIds(flag);
       var fromFlag = flag.environments[sourceEnvironment],
           toFlag =  flag.environments[destinationEnvironment],
           observer = jsonpatch.observe(flag);
@@ -77,7 +88,7 @@ function syncEnvironment (fromKey, toKey) {
         throw new Error('Missing destination environment flag. Did you specify the right project?');
       }
       console.log('Syncing ' + flag.key)
-      copyValues(flag, destinationEnvironment, sourceEnvironment)
+      copyValues(flag, destinationEnvironment, sourceEnvironment);
 
       var diff = jsonpatch.generate(observer);
 
